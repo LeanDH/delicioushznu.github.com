3d1d63d45c3d786fda873f2bfdccf9280e2e1ee5
Lean <872082613@qq.com>
2017-06-04 21:49:21 +0800

    canteen4

--- js/canteen4.js
+++ js/canteen4.js
@@ -30,10 +30,6 @@ function init()
         $(this).children("#l-food-love").addClass('fa-heart');
         $(this).children("#l-food-love").css('color','#ff3232');
         $(this).css('border','1px solid #ff3232');
-		// $('#l-food-love').removeClass('fa-heart-o');    	
-  //   	$('#l-food-love').addClass('fa-heart');
-  //   	$('#l-food-love').css('color','#ff3232');
-  //   	$('.l-food-love').css('border','1px solid #ff3232');
     });
 
     //购物车事件
@@ -41,38 +37,64 @@ function init()
 	    window.location.href="delicious.html";  
 	})
 
-   	//早上菜品切换
-    // var unslider02 = $('.l-food-m-lunbo').unslider({
-    //     dots: false
-    // }),
-    // data02 = unslider02.data('unslider');
-    // $('.unslider-arrow02').click(function() {
-    //         var fn = this.className.split(' ')[1];
-    //         data02[fn]();
-    // });
+   	//菜品切换
+    var unslider02 = $('.l-food-menu-m').unslider({
+        fluid: false,
+        dots: true,
+        speed: 1000
+    }),
+    data02 = unslider02.data('unslider');
+    $('.unslider-arrow02').click(function() {
+            var fn = this.className.split(' ')[1];
+            data02[fn]();
+    });
+
+    var unslider01 = $('.l-food-menu-a').unslider({
+        fluid: false,
+        dots: true,
+        speed: 1000
+    }),
+    data01 = unslider01.data('unslider');
+    $('.unslider-arrow01').click(function() {
+            var fn = this.className.split(' ')[1];
+            data01[fn]();
+    });
+
+    var unslider0 = $('.l-food-menu-n').unslider({
+        fluid: false,
+        dots: true,
+        speed: 1000
+    }),
+    data0 = unslider0.data('unslider');
+    $('.unslider-arrow0').click(function() {
+            var fn = this.className.split(' ')[1];
+            data0[fn]();
+    });    
 
     //早中晚菜单切换
-    var m=0,a=0,n=0;
     $('.l-food-choose-m').click(function(){
-    	$('.l-food-choose-m').css('display','none');
-    	$('.l-food-choose-a').css('display','flex');
-    	$('.l-food-menu-a').css('display','flex');   
-    	$('.l-food-menu-m').css('display','none');
-    	 	
+        $(this).hide();
+        $(this).parent().children('.l-food-choose-a').show();
+        $(this).parent().parent().children('.l-food-menu').children('.l-food-menu-cnta').css('opacity','1');
+        $(this).parent().parent().children('.l-food-menu').children('.l-food-menu-cntm').css('opacity','0');              
+        $(this).parent().parent().children('.l-food-menu').children('.l-food-menu-cnta').css('z-index','1000');
+        $(this).parent().parent().children('.l-food-menu').children('.l-food-menu-cntm').css('z-index','0');     	 	
     });
     $('.l-food-choose-a').click(function(){
-    	$('.l-food-choose-a').css('display','none');
-    	$('.l-food-choose-n').css('display','flex');
-    	$('.l-food-menu-n').css('display','flex');   
-    	$('.l-food-menu-a').css('display','none');
-    	 	
+        $(this).hide();
+        $(this).parent().children('.l-food-choose-n').show();
+        $(this).parent().parent().children('.l-food-menu').children('.l-food-menu-cntn').css('opacity','1');
+        $(this).parent().parent().children('.l-food-menu').children('.l-food-menu-cnta').css('opacity','0');    	
+        $(this).parent().parent().children('.l-food-menu').children('.l-food-menu-cntn').css('z-index','1000');
+        $(this).parent().parent().children('.l-food-menu').children('.l-food-menu-cnta').css('z-index','0');  
     });
     $('.l-food-choose-n').click(function(){
-    	$('.l-food-choose-n').css('display','none');
-    	$('.l-food-choose-m').css('display','flex');
-    	$('.l-food-menu-m').css('display','flex');   
-    	$('.l-food-menu-n').css('display','none');
-    	 	
+        $(this).hide();
+        $(this).parent().children('.l-food-choose-m').show();
+        $(this).parent().parent().children('.l-food-menu').children('.l-food-menu-cntm').css('opacity','1');
+        $(this).parent().parent().children('.l-food-menu').children('.l-food-menu-cntn').css('opacity','0');     	 	
+        $(this).parent().parent().children('.l-food-menu').children('.l-food-menu-cntm').css('z-index','1000');
+        $(this).parent().parent().children('.l-food-menu').children('.l-food-menu-cntn').css('z-index','0');  
     });        
 }
 
